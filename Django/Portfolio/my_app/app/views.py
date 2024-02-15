cea0c9a35ef2fd99bf000c379b1cf7dec80ff247
AQTECH <abdulqadeer0120@gmail.com>
2024-02-15 14:20:19 +0500

    add or delete new fiels

--- Django/Portfolio/my_app/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    context =  {'page':'Home'}
-    return render(request, 'index.html',context)
---
+++ Python/Python Tutorial/main.py
@@ -0,0 +1,43 @@
+# print("Hello World!") # Print any values through print()
+
+# # Data Types
+#     # memory allocation,allocation operation
+# # x=10
+# # print(x)
+# # print(type(x))
+# # primitive  int,float,str,bool
+# # Collection 
+# # List(array)-index-position starting with 0
+
+# # marks=[90,20,30,40]
+# # print(marks)
+# # print(type(marks))
+
+
+# marks=[90,20,30,40]
+# print(marks[2])
+# print(type(marks))
+
+
+# name = input("Enter Your name:- ")
+# age = int(input("Enter Your age:- "))
+# addreess = input("Enter Your Your Address:- ")
+# print("<-------------------------->")
+# print(f"Welcome {name}, your age is {age} and your address is {addreess} ")
+
+
+# a=10
+# b=20
+# temp = a
+# a = b
+# # print(temp)
+# # print(a)
+# b = temp
+# # print(b)
+
+number = eval(input("Enter your Number:- "))
+
+if number >= 0:
+    print("it's positive number")
+else:
+    print("it's a negative number")
